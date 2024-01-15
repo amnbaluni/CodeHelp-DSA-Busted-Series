586be4cce41fef5ca225e1641403b5356f03c2db
Aman Baluni <amanbaluni.abab@gmail.com>
2024-01-15 22:25:38 +0530

    Update maxSum.cpp

--- Lecture105 Maximum Sum of Non-Adjacent Elements/maxSum.cpp
+++ Lecture105 Maximum Sum of Non-Adjacent Elements/maxSum.cpp
@@ -72,39 +72,34 @@ int maximumNonAdjacentSum(vector<int> &nums){
 
 Using Recursion + Tabulation  TC = SC = O(N)
 #include <bits/stdc++.h>
-int solve(vector<int> &nums, int n){
-    //base case
-    if(n<0){
-        return 0;
-    }
-    if(n==0){
-        return nums[0];
-    }
-    
-    //include condition
-    int include = solve(nums, n-2) + nums[n];
-    //exclude condition
-    int exclude = solve(nums, n-1) + 0;
 
-    return max(include, exclude);
+int solve(vector<int> &nums, int stInd, int n){
+    int prev2 = 0;
+    int prev1 = nums[stInd];
+    for(int i=stInd+1; i<=n; i++){
+        int ans = max(nums[i]+prev2, prev1);
+        prev2 = prev1;
+        prev1 = ans;
+    }
+    return prev1;
 } 
 
-int solveTabu(vector<int> &nums){
-    int n = nums.size();
-    vector<int> dp(n,0);
-    dp[0] = nums[0];
-
-    for(int i=1; i<=n; i++){
-        int include = dp[i-2] + nums[i];
-        int exclude = dp[i-1] + 0;
-        dp[i] = max(include, exclude);
-    }
-
-    return dp[n];
-} 
 
 int maximumNonAdjacentSum(vector<int> &nums){
-    return solveTabu(nums);
+    int n = nums.size();
+    if(n == 1){
+        return nums[0];
+    }  
+
+
+// case 1 he robbs 1st house and excludes last
+// 0-based indexing
+    int sum1 = solve(nums, 0, n-1);
+
+// case2 he excludes 1st and robbs last
+// 0-based indexing
+    int sum2 = solve(nums, 1, n-1);
+    return max(sum1, sum2);
 }  
 
 SPACE OPTIMIZATION
