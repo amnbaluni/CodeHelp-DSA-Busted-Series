c06c3cf06bd484c61a83a8df4031c5a90e218c85
Aman Baluni <amanbaluni.abab@gmail.com>
2024-01-22 23:02:27 +0530

    Update minCost.cpp

--- Lecture113 Minimum Cost for Tickets/minCost.cpp
+++ Lecture113 Minimum Cost for Tickets/minCost.cpp
@@ -89,3 +89,28 @@ int minimumCoins(int n, vector<int> days, vector<int> cost) {
 }  
 
 
+Space Optimization  SC=O(1)
+int minimumCoins(int n, vector<int> days, vector<int> cost) {
+  int ans = 0;
+  //create queue for month and week
+  queue<pair<int,int>> month;
+  queue<pair<int,int>> week;
+
+  for(int day:days){
+    //step1: remove expired days
+    while(!month.empty() && month.front().first + 30 <= day){
+      month.pop();
+    }
+    while(!week.empty() && week.front().first + 7 <= day){
+      week.pop();
+    }
+
+    //step2: add cost for current day
+    week.push(make_pair(day, ans+cost[1]));
+    month.push(make_pair(day, ans+cost[2])); 
+
+    //step3: update the answer
+    ans = min(ans+cost[0], min(week.front().second, month.front().second));
+  }
+  return ans;
+}
