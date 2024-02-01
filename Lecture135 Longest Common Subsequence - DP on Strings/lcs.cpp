ef06bbb2f86281f81e01b8c970d52e09ea98455c
Aman Baluni <amanbaluni.abab@gmail.com>
2024-02-01 23:41:54 +0530

    Update lcs.cpp

--- Lecture135 Longest Common Subsequence - DP on Strings/lcs.cpp
+++ Lecture135 Longest Common Subsequence - DP on Strings/lcs.cpp
@@ -90,3 +90,39 @@ int solveTabu(string& text1, string& text2){
     int longestCommonSubsequence(string text1, string text2) {
         return solveTabu(text1, text2);
     }
+
+
+Print Longest Common Subsequence
+class Solution {
+public:
+    string longestCommonSubsequence(string s1, string s2) {
+        int m = s1.size(), n = s2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        string lcs = "";
+        int i = m, j = n;
+        while (i > 0 && j > 0) {
+            if (s1[i - 1] == s2[j - 1]) {
+                lcs = s1[i - 1] + lcs;
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+        
+        return lcs;
+    }
+};
