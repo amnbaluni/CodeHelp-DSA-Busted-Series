13bd0f323cb50b8cb2747da930d30a9ef647c695
Aman Baluni <amanbaluni.abab@gmail.com>
2024-02-21 23:26:27 +0530

    Update gcd.cpp

--- Lecture024 Maths for DSA/gcd.cpp
+++ Lecture024 Maths for DSA/gcd.cpp
@@ -23,16 +23,20 @@ int gcd(int a, int b) {
 }
 
 int main() {
-    
     int a,b;
     cout << "Enter the Values of a and b" << endl;
     cin >> a >> b;
-
     int ans = gcd(a,b);
-
     cout << " The GCD of " << a << " & " << b << " is: " << ans << endl;
-
-
-
     return 0;
+}
+
+Using Recursion
+int gcd (int a,int b){
+   if (b ==0){
+      return a;
+   }
+  else{
+      return gcd(b, a%b);
+      }
 }
