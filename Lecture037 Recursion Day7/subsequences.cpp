87e364f46aa10a5b982c0278352ac44729c00164
Aman Baluni <amanbaluni.abab@gmail.com>
2024-02-27 23:22:20 +0530

    Update subsequences.cpp

--- Lecture037 Recursion Day7/subsequences.cpp
+++ Lecture037 Recursion Day7/subsequences.cpp
@@ -1,23 +1,40 @@
-void solve(vector<string>& ans, string str, string output, int i) {
-    //base case
-    if(i>=str.length()) {
-        if(output.length()>0)
-        ans.push_back(output);
-        return ;
-        }
-    
-    //exclude
-    solve(ans, str, output, i+1);
-    //include
-    output.push_back(str[i]);
-    solve(ans, str, output, i+1);
-}
-
-vector<string> subsequences(string str){
-	
+#include <bits/stdc++.h> 
+#include <vector>
+vector<string> solve(string &str, string &empty, int len, int index){
 	vector<string> ans;
-    string output = "";
-    solve(ans,str,output,0);
-    return ans;
-	
+	//base case
+	if(index == len){
+		ans.push_back(str);
+	}
+	//pick wala case
+	empty = empty + str[index];
+	solve(str, empty, len, index+1);
+	empty.pop_back();
+	solve(str, empty, len, index+1);
+	ans.push_back(empty);
+	return ans;
+}
+vector<string> subsequences(string str){
+	//bit manipulation
+	// int n = str.length();
+	// vector<string>ans;
+	// for (int num = 0; num < (1 << n); num++) {
+	// 	string sub = "";
+	// 	for (int i = 0; i < n; i++) {
+	// 		//check if the ith bit is set or not
+	// 		if (num & (1 << i)) {
+	// 			sub += str[i];
+	// 		}
+	// 	}
+	//Don't include empty string
+	// 	if (sub.length() > 0) {
+	// 		ans.push_back(sub);
+	// 	}
+	// }
+	// sort(ans.begin(), ans.end());
+	// return ans;
+
+	//using recursion
+	string empty = "";
+	return solve(str, empty, str.length(), 0);
 }
