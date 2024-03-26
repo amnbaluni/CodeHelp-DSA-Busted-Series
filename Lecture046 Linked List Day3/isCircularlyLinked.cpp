8ba5e65400257916182cc9520e5ba83f316571ca
Aman Baluni <amanbaluni.abab@gmail.com>
2024-03-27 01:05:38 +0530

    Create isCircularlyLinked.cpp

---
+++ Lecture046 Linked List Day3/isCircularlyLinked.cpp
@@ -0,0 +1,22 @@
+bool isCircular(Node *head) {
+  if (head == NULL)
+    return true; // empty linked list
+  if (head->next == NULL)
+    return false;
+  Node *slow = head;
+  Node *fast = head;
+  while (slow != NULL && fast != NULL) {
+    slow = slow->next;
+    fast = fast->next;
+    if (fast != NULL)
+      fast = fast->next;
+    if (slow == fast) {
+      if (slow == head) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+  return false;
+}
