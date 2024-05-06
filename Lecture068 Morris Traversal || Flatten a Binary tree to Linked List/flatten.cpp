cafd6f10f8b6f432e1b71c5de7da893da268a381
Aman Baluni <amanbaluni.abab@gmail.com>
2024-05-06 23:43:23 +0530

    Update flatten.cpp

--- Lecture068 Morris Traversal || Flatten a Binary tree to Linked List/flatten.cpp
+++ Lecture068 Morris Traversal || Flatten a Binary tree to Linked List/flatten.cpp
@@ -3,12 +3,12 @@ void flatten(Node *root)
         Node* currNode = root;
         while(currNode != NULL){
             if(currNode->left){
-                Node* pred = currNode->left;
+                Node* pred = currNode->left;   /finding predecessor
                 while(pred->right){
-                    pred = pred->right;
+                    pred = pred->right;   //last rightmost node tak jao
                 }
-                pred->right = currNode->right;
-                currNode -> right = currNode->left;
+                pred->right = currNode->right;  //us node ke right ko currNode ke right se link krdo
+                currNode -> right = currNode->left;   //currNode ke right ko left se link krdo
                 //currNode ke left ko null krdo
                 currNode->left = NULL;
             }
