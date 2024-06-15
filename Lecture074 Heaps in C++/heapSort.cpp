9f4254c838796821c3cfa832c76cb2ae95a60fcc
Aman Baluni <amanbaluni.abab@gmail.com>
2024-06-15 13:29:59 +0530

    Update heapSort.cpp

--- Lecture074 Heaps in C++/heapSort.cpp
+++ Lecture074 Heaps in C++/heapSort.cpp
@@ -1,26 +1,47 @@
-#include <bits/stdc++.h> 
-void heapify(vector<int>& arr, int n, int i){
-    int largest = i;
-    int left = 2*i;
-    int right = 2*i + 1;
-    if(left <= n && arr[largest] < arr[left]){
-        largest = left;
-    }
-    if(right <= n && arr[largest] < arr[right]){
-        largest = right;
-    }
-    if(largest != i){
-        swap(arr[largest], arr[i]);
-        heapify(arr, n, largest);
-    }
-}
+https://leetcode.com/problems/sort-an-array/
 
-vector<int> heapSort(vector<int>& arr, int n) {
-	int size = n;
-    while (size > 1) {
-        swap(arr[1], arr[size]);
-        size--;
-		heapify(arr,size,1);
+class Solution {
+public:
+    void maxHeapify(vector<int>& nums, int n, int i){
+        int largest = i;
+        //0 based indexing
+        int left = (2 * i) + 1; 
+        int right = (2 * i) + 2;
+
+        if(left < n && nums[left] > nums[largest])
+            largest = left;
+        if(right < n && nums[right] > nums[largest])
+            largest = right;
+        if(largest != i){
+            swap(nums[largest], nums[i]);
+            maxHeapify(nums, n, largest);
+        }
     }
-	arr;
-}
+
+    void heapSort(vector<int>& nums, int n){
+        for(int i = n/2-1; i >= 0; i--)
+            maxHeapify(nums, n, i);
+        for(int i = n-1; i >= 0; i--){
+            swap(nums[0], nums[i]);
+            maxHeapify(nums, i, 0);
+        }
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        // heapSort(nums, nums.size());
+        // return nums;
+
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int i=0; i<nums.size(); i++){
+            pq.push(nums[i]);
+        }
+        int i = 0;
+        while(!pq.empty()){
+            int top = pq.top();
+            nums[i] = top;
+            pq.pop();
+            i++;
+        }
+        return nums;
+    }
+};
