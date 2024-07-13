550f544518f654da117405615a1e23e9efefee1a
Aman Baluni <amanbaluni.abab@gmail.com>
2024-07-13 10:27:03 +0530

    Update maxSum.cpp

--- Lecture105 Maximum Sum of Non-Adjacent Elements/maxSum.cpp
+++ Lecture105 Maximum Sum of Non-Adjacent Elements/maxSum.cpp
@@ -1,3 +1,5 @@
+https://www.naukri.com/code360/problems/maximum-sum-of-non-adjacent-elements_843261
+
 Using Recursion
 #include <bits/stdc++.h>
 int solve(vector<int> &nums, int n){
@@ -18,29 +20,12 @@ int solve(vector<int> &nums, int n){
 } 
 int maximumNonAdjacentSum(vector<int> &nums){
     int n = nums.size();
+    //startin from last index
     int ans = solve(nums, n-1);
     return ans;
 }
 
-Using Recursion + Memoization  TC = SC = O(N)
-#include <bits/stdc++.h>
-int solve(vector<int> &nums, int n){
-    //base case
-    if(n<0){
-        return 0;
-    }
-    if(n==0){
-        return nums[0];
-    }
-    
-    //include condition
-    int include = solve(nums, n-2) + nums[n];
-    //exclude condition
-    int exclude = solve(nums, n-1) + 0;
-
-    return max(include, exclude);
-} 
-
+Using Recursion + Memoization  TC = O(N); SC = O(n)+O(n)
 int solveMemo(vector<int> &nums, int n, vector<int> &dp){
     //base case
     if(n<0){
@@ -72,39 +57,26 @@ int maximumNonAdjacentSum(vector<int> &nums){
 
 Using Recursion + Tabulation  TC = SC = O(N)
 #include <bits/stdc++.h>
-
-int solve(vector<int> &nums, int stInd, int n){
-    int prev2 = 0;
-    int prev1 = nums[stInd];
-    for(int i=stInd+1; i<=n; i++){
-        int ans = max(nums[i]+prev2, prev1);
-        prev2 = prev1;
-        prev1 = ans;
-    }
-    return prev1;
-} 
-
-
-int maximumNonAdjacentSum(vector<int> &nums){
-    int n = nums.size();
-    if(n == 1){
-        return nums[0];
-    }  
-
-
-// case 1 he robbs 1st house and excludes last
-// 0-based indexing
-    int sum1 = solve(nums, 0, n-1);
-
-// case2 he excludes 1st and robbs last
-// 0-based indexing
-    int sum2 = solve(nums, 1, n-1);
-    return max(sum1, sum2);
-}  
-
-SPACE OPTIMIZATION
 int solveTabu(vector<int> &nums){
-    //space optimization - doing with dp array
+    int n = nums.size();
+    vector<int> dp(n, 0);
+    dp[0] = nums[0];
+    for(int i=1; i<n; i++){
+        int include = dp[i-2] + nums[i];
+        int exclude = dp[i-1] + 0;
+        dp[i] = max(include, exclude);
+    }
+    return dp[n-1];
+}
+
+int maximumNonAdjacentSum(vector<int> &nums) {
+    return solveTabu(nums);
+}
+
+
+SPACE OPTIMIZATION - TC = (n)  SC = O(1)
+int spaceOpti(vector<int> &nums){
+    //space optimization - doing without dp array
     int n = nums.size();
     int prev2 = 0; 
     int prev1 = nums[0];
@@ -116,6 +88,29 @@ int solveTabu(vector<int> &nums){
         prev2 = prev1;
         prev1 = ans;
     }
-
     return prev1;
 } 
+int maximumNonAdjacentSum(vector<int> &nums) {
+    return spaceOpti(nums);
+}
+************************************************************
+//Starting from first index    
+int solveMemo(vector<int> &nums, int i, vector<int> &dp){
+    //base case
+    if(i >= nums.size()){
+        return 0;
+    }
+    if(dp[i] != -1) return dp[i];
+    //include condition
+    int include = solveMemo(nums, i+2, dp) + nums[i];
+    //exclude condition
+    int exclude = solveMemo(nums, i+1, dp) + 0;
+
+    return dp[i] = max(include, exclude);
+} 
+
+int maximumNonAdjacentSum(vector<int> &nums){
+    int n = nums.size();
+    vector<int> dp(n, -1);
+    return solveMemo(nums, 0, dp);
+}
