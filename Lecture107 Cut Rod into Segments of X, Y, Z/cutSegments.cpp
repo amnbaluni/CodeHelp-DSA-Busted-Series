ecabec292186dcbc24615258452a6b1c1943a5e1
Aman Baluni <amanbaluni.abab@gmail.com>
2024-07-14 18:54:08 +0530

    Update cutSegments.cpp

--- Lecture107 Cut Rod into Segments of X, Y, Z/cutSegments.cpp
+++ Lecture107 Cut Rod into Segments of X, Y, Z/cutSegments.cpp
@@ -1,3 +1,5 @@
+https://www.naukri.com/code360/problems/cut-into-segments_1214651
+
 Recursion TC = exponential
 #include <limits.h>
 int solve(int n, int x, int y, int z){
@@ -6,7 +8,7 @@ int solve(int n, int x, int y, int z){
 		return 0;
 	}
 	if(n<0){
-		return INT_MIN;
+		return INT_MIN;   //ans nikalna is impossible
 	}
 
 	int a = solve(n-x,x,y,x) + 1; 
@@ -29,51 +31,51 @@ int cutSegments(int n, int x, int y, int z) {
 
 Recursion + Memoization   TC = O(N)  SC = O(N)+O(N)
 #include <limits.h>
-int solveMemo(int n, int x, int y, int z, vector<int> &dp){
-	//base case
-	if(n==0){
-		return 0;
-	}
-	if(n<0){
-		return INT_MIN;
-	}
-	if(dp[n] != -1){
-		return dp[n];
-	}
+int solveMemo(int n, int x, int y, int z, vector<int> &dp) {
+  if (n == 0) {
+    return 0;
+  }
+  if (n < 0) {
+    return INT_MIN;   //ans nikalna is impossible
+  }
+  if (dp[n] != -1) {
+    return dp[n];
+  }
 
-	int a = solveMemo(n-x,x,y,x,dp) + 1; 
-	int b = solveMemo(n-y,x,y,x,dp) + 1;
-	int c = solveMemo(n-z,x,y,x,dp) + 1;
+  int a = solveMemo(n - x, x, y, z, dp) + 1;
+  int b = solveMemo(n - y, x, y, z, dp) + 1;
+  int c = solveMemo(n - z, x, y, z, dp) + 1;
+  int ans = max(a, max(b, c));
 
-	dp[n] = max(a,max(b,c));
-	return dp[n];
+  dp[n] = ans;
+  return ans;
 }
 int cutSegments(int n, int x, int y, int z) {
-	vector<int> dp(n+1, -1);
-	int ans = solveMemo(n,x,y,z,dp);
-	if(ans<0){
-		return 0;
-	}
-	else{
-		return ans;
-	}
+  vector<int> dp(n + 1, -1);   //0 based indexing
+  int ans = solveMemo(n, x, y, z, dp);
+  if (ans < 0) {
+    return 0;
+  }
+  return ans;
 }
+
+
 Recursion + Tabulation TC = SC = O(N)
 #include <limits.h>
 
 int solveTabu(int n, int x, int y, int z){
-	vector<int> dp(n+1, -1);
-    dp[0] = 0;
+	vector<int> dp(n+1, INT_MIN);
+        dp[0] = 0;
 	for(int i=1; i<=n; i++){
-       //check for invalid index
-	   if(i-x >= 0 && dp[i-x]!=-1){
-           dp[i] = max(dp[i], dp[i-x]+1);
+           //check for invalid index
+	   if(i-x >= 0){
+              dp[i] = max(dp[i], dp[i-x]+1);
 	   }
-	   if(i-y >= 0 && dp[i-y]!=-1){
-           dp[i] = max(dp[i], dp[i-y]+1);
+	   if(i-y >= 0){
+              dp[i] = max(dp[i], dp[i-y]+1);
 	   } 
-	   if(i-z >= 0 && dp[i-z]!=-1){
-           dp[i] = max(dp[i], dp[i-z]+1);
+	   if(i-z >= 0){
+              dp[i] = max(dp[i], dp[i-z]+1);
 	   }
 	}
 	if(dp[n]<0){
@@ -84,7 +86,6 @@ int solveTabu(int n, int x, int y, int z){
 	}
 }
 int cutSegments(int n, int x, int y, int z) {
-	vector<int> dp(n+1, -1);
 	int ans = solveTabu(n,x,y,z);
 	if(ans<0){
 		return 0;
