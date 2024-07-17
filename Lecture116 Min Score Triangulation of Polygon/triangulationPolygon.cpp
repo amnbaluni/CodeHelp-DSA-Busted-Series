fd38fe6b04e09280e8f7c5251769053df74a459f
Aman Baluni <amanbaluni.abab@gmail.com>
2024-07-17 22:53:23 +0530

    Update triangulationPolygon.cpp

--- Lecture116 Min Score Triangulation of Polygon/triangulationPolygon.cpp
+++ Lecture116 Min Score Triangulation of Polygon/triangulationPolygon.cpp
@@ -46,7 +46,7 @@ int solveTabu(vector<int>& v){
        int n = v.size();
        vector<vector<int>> dp(n, vector<int>(n,0));
        for(int i=n-1; i>=0; i--){
-           for(int j=i+2; j<n; j++){
+           for(int j=i+2; j<n; j++){  //imp -> start j from i+2
                int ans = INT_MAX;
                for(int k=i+1; k<j; k++){
                ans = min(ans, v[i]*v[j]*v[k] + dp[i][k] + dp[k][j]);
