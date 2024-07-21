6993da5c52e7b71ea1c3e92b5186318dcbbccdb5
Aman Baluni <amanbaluni.abab@gmail.com>
2024-07-21 12:34:34 +0530

    Update topoSort.cpp

--- Lecture091 Topological Sort using BFS - Kahn's Algorithm/topoSort.cpp
+++ Lecture091 Topological Sort using BFS - Kahn's Algorithm/topoSort.cpp
@@ -49,3 +49,36 @@ vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
 Time Complexity - O(N+E)
 Space Complexity - O(N+E)
   
+******************************************************************************
+=> If I have already given adj list in input
+https://www.geeksforgeeks.org/problems/topological-sort/1
+vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	   int indegree[V] = {0};
+	   for(int i=0; i<V; i++){
+	       for(auto it : adj[i]){
+	            indegree[it]++;
+	       }
+	   }
+	   queue<int> q;
+	   //put all vertices in queue having indgree as 0
+	   for(int i=0; i<V; i++){
+	       if(indegree[i] == 0){
+	           q.push(i);
+	       }
+	   }
+	   vector<int> topo_sort;
+	   while(!q.empty()){
+	       int node = q.front();
+	       q.pop();
+	       topo_sort.push_back(node);
+	       for(auto it : adj[node]){
+	           indegree[it]--;
+	           if(indegree[it] == 0){
+	               q.push(it);
+	           }
+	       }
+	   }
+	   return topo_sort;
+	}    
+    
